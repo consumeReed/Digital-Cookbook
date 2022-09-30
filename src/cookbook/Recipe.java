21ccdc3f856a03b372878566b8a59eb622fbf14f
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2022-09-30 16:17:29 -0400

    Initial Recipe.java
    I added two basic fields (name and size) and just a basic constructor method

--- src/cookbook/Recipe.java
+++ src/cookbook/Recipe.java
@@ -1,7 +1,11 @@
 package cookbook;
 
 public class Recipe {
-	//test Sean Kwak
-	// test1
-
+	private String recipe_name;
+	private int people_feeds;
+	
+	public Recipe(String recipe_name, int people_feeds) {
+		this.recipe_name = recipe_name;
+		this.people_feeds = people_feeds;
+	}
 }
