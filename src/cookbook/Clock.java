f4eeeb39be0996864cf21b69d7532a665ee9df50
Sreypich Heng <jerrybee122@gmail.com>
2022-10-25 18:50:38 -0400

    Update clock

---
+++ src/cookbook/Clock.java
@@ -0,0 +1,107 @@
+package cookbook;
+
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+/** 
+ * Timer for when user want to set time while cooking 
+ */
+
+public class Clock
+{
+	
+	int timePassed = 0;
+	
+	Timer timer = new Timer();
+	
+	TimerTask task = new TimerTask() {
+		
+		public void run() {
+			timePassed++;
+			//System.out.println(new Date ());
+			System.out.println("Time started: " + timePassed);
+		}
+	};
+	
+	
+	public static void main(String[] args) {
+		Clock clock = new Clock();
+		clock.start();
+	}
+	
+	public void start() {
+		System.out.println(new Date ());
+		timer.scheduleAtFixedRate(task, 1000, 1000);
+		completeTask();
+		//System.out.println("Timer finished: " + new Date());
+	}
+	
+	private void completeTask()
+	{
+		try
+		{
+			// assume it takes 20 seconds 
+			Thread.sleep(20000);
+		}
+		catch(InterruptedException ex)
+		{
+			ex.printStackTrace();
+		}
+		//cancel after sometime
+		try
+		{
+			Thread.sleep(120000);
+		}
+			catch(InterruptedException ex)
+		{
+				ex.printStackTrace();
+			}
+	}
+	
+	/**public void start()
+	{
+		{
+			System.out.println("Time started: " + new Date());
+			completeTask();
+			System.out.println("Timer finished: " + new Date());
+		}
+	}
+
+	private void completeTask()
+	{
+		try
+		{
+			// assume it takes 20 seconds 
+			Thread.sleep(20000);
+		}
+		catch(InterruptedException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		TimerTask timerTask = new Clock();
+		//
+		Timer timer = new Timer(true);
+		timer.scheduleAtFixedRate(timerTask, 0, 10*1000);
+		System.out.println("TimerTask started");
+		
+		//cancel after sometime
+		try
+		{
+			Thread.sleep(120000);
+		}
+		catch(InterruptedException ex)
+		{
+			ex.printStackTrace();
+		}
+
+		
+	} **/
+}
