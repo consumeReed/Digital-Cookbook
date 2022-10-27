ba176b43ce0195de16eb48220458f5f910a58f26
MatthewMajews <majews37@students.rowan.edu>
2022-10-26 20:30:21 -0400

    Updated UI
    fixed the ability to hit "enter" to submit recipe in the search bar
    
    commented out old version of UI, using JPanel now for ease of customization when it comes to dropboxes, pictures, etc

--- src/cookbook/UserInterface.java
+++ src/cookbook/UserInterface.java
@@ -9,20 +9,23 @@ import java.awt.event.KeyListener;
 import javax.swing.*;
 
 
-public class UserInterface implements KeyListener{
+public class UserInterface {
 
+	private JPanel initial_panel;
+	
 	private JFrame initial_frame;
 	private JButton open_recipe_add;
 	private JTextField recipe_search_bar;
-	private JButton recipe_search; 
+	private JButton recipe_search;
 	private JButton search_bar_clear;
 	private JLabel test_label;
 	
 	private JFrame add_recipe_frame;
 	
 	private ActionListener open_recipe_frame;
-	private ActionListener search_recipe_book; 
+	private ActionListener search_recipe_book;
 	private ActionListener clear_search_bar;
+	private KeyListener enter;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,38 +47,50 @@ public class UserInterface implements KeyListener{
 	private void initialize() {
 		setUpButtonListeners();
 		
+		initial_panel = new JPanel();
+		
 		initial_frame = new JFrame("Digital Recipe Book");						// Creates the frame that will be shown when code is ran
-		initial_frame.setBounds(100, 100, 1080, 720);							// Sets the size of the frame (currently 1080 x 720 pixels) (100, 100 represents where on the device the frame will be created ie at pos (100, 100))
+		initial_frame.add(initial_panel);
+//		initial_frame.setBounds(100, 100, 1080, 720);							// Sets the size of the frame (currently 1080 x 720 pixels) (100, 100 represents where on the device the frame will be created ie at pos (100, 100))
 		initial_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			// When "X" in top right corner is click, this makes it so the frame closes
-		initial_frame.getContentPane().setLayout(null);							// Sets layout of the frame (Useful )
+//		initial_frame.getContentPane().setLayout(null);							// Sets layout of the frame (Useful )
+		initial_frame.setSize(1080, 720);
+		initial_frame.setLocation(100, 100);
 		
 		open_recipe_add = new JButton("Add Recipe");							// Button to add recipe (When clicked opens new window)
 		open_recipe_add.setSize(300, 20);
 		open_recipe_add.addActionListener(open_recipe_frame);
-		initial_frame.getContentPane().add(open_recipe_add);
+//		initial_frame.getContentPane().add(open_recipe_add);
 		open_recipe_add.setLocation(0, 0);
+		initial_panel.add(open_recipe_add);
 		
 		recipe_search = new JButton("Search");									// Button to search for recipe (When clicked gets text from recipe_search_bar)
-		recipe_search.setSize(100, 20);
+//		recipe_search.setSize(100, 20);
 		recipe_search.addActionListener(search_recipe_book);
-		initial_frame.getContentPane().add(recipe_search);
-		recipe_search.setLocation(790, 30);
+//		initial_frame.getContentPane().add(recipe_search);
+//		recipe_search.setLocation(790, 30);
+		initial_panel.add(recipe_search);
 		
 		recipe_search_bar = new JTextField();									// Text Field for user to enter something to search for
-		recipe_search_bar.setSize(500, 20);
-		initial_frame.getContentPane().add(recipe_search_bar);
-		recipe_search_bar.setLocation(290, 30);
+//		recipe_search_bar.setSize(500, 20);
+//		initial_frame.getContentPane().add(recipe_search_bar);
+//		recipe_search_bar.setLocation(290, 30);
+		recipe_search_bar.setPreferredSize(new Dimension(300, 20));
+		initial_panel.add(recipe_search_bar);
+		recipe_search_bar.addKeyListener(enter);
 		
 		search_bar_clear = new JButton("X");									// Button to clear the recipe_search_bar (Avoids having to delete what's currently in search bar)
-		search_bar_clear.setSize(50, 20);
+//		search_bar_clear.setSize(50, 20);
 		search_bar_clear.addActionListener(clear_search_bar);
-		initial_frame.getContentPane().add(search_bar_clear);
-		search_bar_clear.setLocation(880, 30);
+//		initial_frame.getContentPane().add(search_bar_clear);
+//		search_bar_clear.setLocation(880, 30);
+		initial_panel.add(search_bar_clear);
 		
 		test_label = new JLabel();
-		test_label.setSize(500, 40);
-		initial_frame.getContentPane().add(test_label);
-		test_label.setLocation(290, 50);
+//		test_label.setSize(500, 40);
+//		initial_frame.getContentPane().add(test_label);
+//		test_label.setLocation(290, 50);
+		initial_panel.add(test_label);
 	}
 	
 	public void add_recipe_frame_maker() {
@@ -112,31 +127,31 @@ public class UserInterface implements KeyListener{
 				recipe_search_bar.setText("");
 			}
 		};
-	
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		 }
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getKeyCode()==KeyEvent.VK_ENTER)// clicking enter button for search
-		 { 
-				if(test_label.getText() == "") {
-					test_label.setText(recipe_search_bar.getText());			// recipe_search_bar.getText() is used to get text thats stored in recipe_search_bar at time of button click
-				}
-				else {
-					test_label.setText(recipe_search_bar.getText());
-				
-	
-	}}}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
 		
+		enter = new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode()==KeyEvent.VK_ENTER) {						// clicking enter button for search
+					if(test_label.getText() == "") {
+						test_label.setText(recipe_search_bar.getText());
+					}
+					else {
+						test_label.setText(recipe_search_bar.getText());
+					}
+				}
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				
+			}
+		};
 	}
+	
+	
 }
