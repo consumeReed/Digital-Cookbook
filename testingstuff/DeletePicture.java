8cb2d598109649f4ac4c7a8c6a0b83de5109e266
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2022-11-02 18:17:08 -0400

    Test Delete Pictures
    Added a temp picture delete method/class

---
+++ testingstuff/DeletePicture.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class DeletePicture {
+	public static void main(String[] args) {
+		/**File file = new File("userdata\\pictureName.whateverExtension");
+		
+		if (file.delete()) {
+            System.out.println("File deleted successfully");
+        }
+        else {
+            System.out.println("Failed to delete the file");
+        }**/
+	}
+}
