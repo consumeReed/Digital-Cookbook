b435477a6cf871ed3f5118f9366fc42702d06e13
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2022-11-05 13:04:30 -0400

    Delete Picture Errors
    Testing for Phantom Files

--- testingstuff/DeletePicture.java
+++ testingstuff/DeletePicture.java
@@ -1,6 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
 import java.io.*;
-public class DeletePicture {
-	public static void main(String[] args) {
+import java.nio.file.*;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class DeletePicture extends JFrame implements ActionListener{
 		/**File file = new File("userdata\\pictureName.whateverExtension");
 		
 		if (file.delete()) {
@@ -9,5 +16,43 @@ public class DeletePicture {
         else {
             System.out.println("Failed to delete the file");
         }**/
+		
+	JButton button;
+	DeletePicture() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		
+		button = new JButton("Select File");
+		button.addActionListener(this);
+		
+		this.add(button);
+		this.pack();
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==button) {
+			
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.addChoosableFileFilter(new ImageFilter());
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.setCurrentDirectory(new File(".")); //sets directory
+            int response = fileChooser.showOpenDialog(null); //select file to open
+            if(response == JFileChooser.APPROVE_OPTION) {
+            	File src = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            	Path a = Paths.get(src.getPath());
+            	try {
+            		Files.delete(a);
+            	} catch (Exception e2) {
+            		e2.printStackTrace();
+            	}
+            	}
+			}
+	}
+	
+	public static void Main(String[] args) {
+		//full method test.
+		new DeletePicture();
 	}
 }
